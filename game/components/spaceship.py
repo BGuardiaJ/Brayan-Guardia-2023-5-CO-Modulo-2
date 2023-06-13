76187257b478c0c4a0f93ad1b8fa356044fa38f4
BGuardiaJ <bdguardia1@gmail.com>
2023-06-13 17:29:07 -0500

    First-Class-Homework

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -28,12 +28,14 @@ class Spaceship(Sprite):
           self.move_down()
             
     def move_left(self):
-      if self.rect.left > 0:
         self.rect.x -=10
+        if self.rect.x <= 0:
+            self.rect.x = 1100
         
     def move_right(self):
-      if self.rect.right < SCREEN_WIDTH:
         self.rect.x +=10
+        if self.rect.x >= 1100:
+            self.rect.x = 0
         
     def move_up(self):
       if self.rect.y > self.HALF_SCREEN_HEIGHT:
